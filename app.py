780e21a9fe703ee6c209d878dc5405ed195805ca
Patrick Widmer <padi.widmer@gmail.com>
2018-06-25 22:10:06 +0200

    Clean up

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ GAS_LIMIT = 90000
 ENDPOINT_URI = 'http://localhost:8545'
 PRIVATE_KEY = '0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318'
 
-w3 = Web3(HTTPProvider(ENDPOINT_URI))
-gas_price = w3.eth.gasPrice
-coinbase = w3.eth.coinbase
+web3 = Web3(HTTPProvider(ENDPOINT_URI))
+gas_price = web3.eth.gasPrice
+coinbase = web3.eth.coinbase
 
 
 def sign_transaction(transaction, key):
-    return w3.eth.account.signTransaction(transaction, key)
+    return web3.eth.account.signTransaction(transaction, key)
 
 
 def transaction_count(account):
-    return w3.eth.getTransactionCount(account)
+    return web3.eth.getTransactionCount(account)
 
 
 def send_raw_transaction(raw_transaction):
-    return w3.eth.sendRawTransaction(raw_transaction)
+    return web3.eth.sendRawTransaction(raw_transaction)
 
 
 def to_checksum_address(address):
-    return w3.toChecksumAddress(address)
+    return web3.toChecksumAddress(address)
 
 
 def transaction(
