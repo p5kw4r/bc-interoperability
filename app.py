14bbe44f2760ca6bafb03e2ac624c1cb7dccbd75
Patrick Widmer <padi.widmer@gmail.com>
2018-06-26 09:14:50 +0200

    Clean up

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def transaction(
 
 
 def main(argv=sys.argv):
-    signed_tx = sign_transaction(transaction(), private_key)
+    tx = transaction()
+    signed_tx = sign_transaction(tx, private_key)
     print(signed_tx)
     # send_raw_transaction(signed_tx.rawTransaction)  # sender doesn't have enough funds to send tx
 
