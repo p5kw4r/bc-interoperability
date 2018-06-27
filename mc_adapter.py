3631ca714dfe25390d6b605db404d087456382f8
Patrick Widmer <padi.widmer@gmail.com>
2018-06-27 16:34:45 +0200

    Add documentation for credentials

--- mc_adapter.py
+++ mc_adapter.py
@@ -5,9 +5,13 @@ from adapter import encoding
 host = 'localhost'
 port = '7324'
 
+# The API credentials for each blockchain are stored in the
+# ~/.multichain/[chain-name]/multichain.conf
 rpcuser = 'multichainrpc'
 rpcpassword = 'GkHfnch8QBgqvZJeMLyb57h42h6TZREr25Uhp5iZ8T2E'
 
+# The private key can be found by running `dumpprivkey [address]` command in
+# interactive mode
 default_address = '1RuG62c89Vk1V6psGhtAwywan9mWsvFvBv2cLM'
 private_key = 'VAUWVB6KStqzemdzXqak77cbkaz6tyYyRbcG3pqBcpP2xNFzAvT8bt2E'
 
