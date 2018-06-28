d83601fc472b109f2b7749a225124b0990e1695c
Patrick Widmer <padi.widmer@gmail.com>
2018-06-28 08:45:06 +0200

    Extract rpc client outside of adapter class to allow abstract base class and to prevent TypeError: can't instantiate abstract class MCAdapter with abstract methods client

--- mc_adapter.py
+++ mc_adapter.py
@@ -1,6 +1,6 @@
 from binascii import hexlify, unhexlify
 from mcrpc import RpcClient
-from adapter import default_amount, encoding
+from adapter import Adapter, default_amount, encoding
 
 host = 'localhost'
 port = '7324'
@@ -10,22 +10,23 @@ port = '7324'
 rpcuser = 'multichainrpc'
 rpcpassword = 'GkHfnch8QBgqvZJeMLyb57h42h6TZREr25Uhp5iZ8T2E'
 
+client = RpcClient(host, port, rpcuser, rpcpassword)
+
 # The private key can be found by running `dumpprivkey [address]` command in
 # interactive mode
 default_address = '1RuG62c89Vk1V6psGhtAwywan9mWsvFvBv2cLM'
 private_key = 'VAUWVB6KStqzemdzXqak77cbkaz6tyYyRbcG3pqBcpP2xNFzAvT8bt2E'
 
 
-class MCAdapter:
-    client = RpcClient(host, port, rpcuser, rpcpassword)
-
+class MCAdapter(Adapter):
     def retrieve(self, transaction_hash):
         tx = self.get_raw_transaction(transaction_hash)
         data_hex = self.extract_data(tx)
         return self.to_text(data_hex)
 
-    def get_raw_transaction(self, transaction_hash):
-        return self.client.getrawtransaction(transaction_hash, verbose=1)
+    @staticmethod
+    def get_raw_transaction(transaction_hash):
+        return client.getrawtransaction(transaction_hash, verbose=1)
 
     @staticmethod
     def extract_data(transaction):
@@ -47,25 +48,27 @@ class MCAdapter:
         text_bytes = bytes(text, encoding=encoding)
         return hexlify(text_bytes)
 
+    @staticmethod
     def create_raw_send_from(
-            self,
             data_hex,
             sender=default_address,
             recipient=default_address,
             amount=default_amount):
-        return self.client.createrawsendfrom(
+        return client.createrawsendfrom(
             sender,
             {recipient: amount},
             [data_hex])
 
-    def sign_raw_transaction(self, transaction_hex):
+    @staticmethod
+    def sign_raw_transaction(transaction_hex):
         parent_outputs = []
-        signed = self.client.signrawtransaction(
+        signed = client.signrawtransaction(
             transaction_hex,
             parent_outputs,
             [private_key])
         if signed['complete']:
             return signed['hex']
 
-    def send_raw_transaction(self, transaction_hash):
-        return self.client.sendrawtransaction(transaction_hash)
+    @staticmethod
+    def send_raw_transaction(transaction_hash):
+        return client.sendrawtransaction(transaction_hash)
