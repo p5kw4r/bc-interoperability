d249cae68de6dc3f51fcad22df9a183b5416ad72
Patrick Widmer <padi.widmer@gmail.com>
2018-07-02 10:43:09 +0200

    Clean up

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ Adapter = {
 
 
 def store(text, blockchain_id):
-    transaction_hash = Adapter[blockchain_id].store(text)
+    adapter = Adapter[blockchain_id]
+    transaction_hash = adapter.store(text)
     return transaction_hash
 
 
 def retrieve(transaction_hash, blockchain_id):
-    text = Adapter[blockchain_id].retrieve(transaction_hash)
+    adapter = Adapter[blockchain_id]
+    text = adapter.retrieve(transaction_hash)
     return text
